b5f13b5ce31b048e43faf812f058be544b41c0e5
PecorinaAmaga01! <01ramazza@gmail.com>
2024-05-02 11:51:27 -0300

    Refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import './App.css';
+import Home from './pages/home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        {/* <Route path='' element={} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
